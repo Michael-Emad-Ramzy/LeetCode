7862202e13ef70542df2896100cf22b8a087be7a
Michael Emad <139220121+Michael-Emad-Ramzy@users.noreply.github.com>
2026-01-13 21:36:09 +0200

    Time: 3 ms (59.18%), Space: 56.4 MB (37.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j] === target){
-                return [i, j];
-            }
+    // My First answer
+    //
+    // for(let i=0;i<nums.length;i++){
+    //     for(let j=i+1;j<nums.length;j++){
+    //         if(nums[i]+nums[j] === target){
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+
+    // the better answer 
+
+    const map = {}
+    for(let i=0 ; i<nums.length ;i++){
+        if(target - nums[i] in map){
+            return [map[target - nums[i]],i]
+        }else{
+        map[nums[i]] = i;
         }
     }
 };
